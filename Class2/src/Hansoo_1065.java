e893327b13aeaf5c560183ef7be0993aef15e56b
Lee JongSu <81428357+Lee-JongSoo@users.noreply.github.com>
2022-02-25 17:43:28 +0900

    Class 2

---
+++ Class2/src/Hansoo_1065.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Hansoo_1065 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+
+        if (n < 100) {
+            count = n;
+        } else {
+            count = 99;
+            if (n == 100) n = 999;
+            for (int i = 100; i <= n; i++) {
+                int h = i / 100;
+                int t = (i / 10) % 10;
+                int o = i % 10;
+                if ((h - t) == (t - o)) count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
