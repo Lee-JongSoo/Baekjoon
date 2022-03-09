4612d92180f7a21fe0b6da7ede1e877d461b28ae
Lee JongSu <81428357+Lee-JongSoo@users.noreply.github.com>
2022-03-09 21:44:41 +0900

    Class 2

---
+++ Class2/src/LANCut_1654.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class LANCut_1654 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
+        int n = sc.nextInt();
+
+        int[] arr = new int[k];
+        for (int i = 0; i < k; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        long max = Arrays.stream(arr).max().getAsInt();
+        max++;
+        long min = 0;
+        long mid = 0;
+
+        while (min < max) {
+            long count = 0;
+            mid = (max + min) / 2;
+
+            for (int i = 0; i < arr.length; i++) {
+                count += (arr[i] / mid);
+            }
+
+            if (count < n) max = mid;
+            else min = mid + 1;
+        }
+        System.out.println(min - 1);
+    }
+}
