cc5430f89c8a09ec2e16d1d2b8831c25cbc83f8e
Lee JongSu <81428357+Lee-JongSoo@users.noreply.github.com>
2022-03-15 21:01:35 +0900

    Study LIS Algorithm

--- Class2/src/LIS5_14003.java
+++ Class2/src/LIS5_14003.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class LIS5_14003 {
@@ -10,18 +11,21 @@ public class LIS5_14003 {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
+        int[] arr = new int[n + 1];
         ArrayList<Integer> lis = new ArrayList<>();
         lis.add(Integer.MIN_VALUE);
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 0; i < n; i++) {
+        int[] indexArr = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
             if (lis.get(lis.size() - 1) < arr[i]) {
                 lis.add(arr[i]);
+                indexArr[i] = lis.size() - 1;
             } else {
                 int left = 1;
                 int right = lis.size() - 1;
@@ -35,11 +39,23 @@ public class LIS5_14003 {
                     }
                 }
                 lis.set(right, arr[i]);
+                indexArr[i] = right;
             }
         }
-        System.out.println(lis.size() - 1);
-        for (int i = 1; i < lis.size() ; i++) {
-            System.out.print(lis.get(i) + " ");
+        int index = lis.size() - 1;
+
+        System.out.println(index);
+
+        Stack<Integer> stack = new Stack<>();
+        for (int i = n; i > 0; i--) {
+            if (indexArr[i] == index) {
+                index--;
+                stack.push(arr[i]);
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop() + " ");
         }
     }
 }
