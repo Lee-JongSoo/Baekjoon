b673a5882be4a3d84c67fae28c6b05c6f86ce931
Lee JongSu <81428357+Lee-JongSoo@users.noreply.github.com>
2022-03-18 12:20:55 +0900

    ArrayQueue(Circular Queue) implement

---
+++ Class2/src/Algorithm/circularqueue/SortingTest2.java
@@ -0,0 +1,44 @@
+package Algorithm.circularqueue;
+
+import java.util.Comparator;
+
+public class SortingTest2 {
+    public static void main(String[] args) {
+
+        ArrayQueue<Student> q = new ArrayQueue<>();
+
+        q.offer(new Student("김자바", 92));
+        q.offer(new Student("이시플", 72));
+        q.offer(new Student("조시샵", 98));
+        q.offer(new Student("파이손", 51));
+
+        q.sort(customComp);	// Comparator을 넘겨준다.
+
+        for(Object a : q.toArray()) {
+            System.out.println(a);
+        }
+    }
+
+    // 사용자 설정 comparator(비교기)
+    static Comparator<Student> customComp = new Comparator<Student>() {
+        @Override
+        public int compare(Student o1, Student o2) {
+            return o2.score - o1.score;
+        }
+    };
+
+}
+
+class Student {
+    String name;
+    int score;
+
+    Student(String name, int score){
+        this.name = name;
+        this.score = score;
+    }
+
+    public String toString() {
+        return "이름 : " + name + "\t성적 : " + score;
+    }
+}
