3d8db869a7c809fa157c0b5acee502627a35b60d
Lee JongSu <81428357+Lee-JongSoo@users.noreply.github.com>
2022-03-24 18:48:56 +0900

    Class 2

--- Class2/src/Snail_2869.java
+++ Class2/src/Snail_2869.java
@@ -1,74 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
+public class Snail_2869 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int up = Integer.parseInt(st.nextToken());
+        int down = Integer.parseInt(st.nextToken());
+        int length = Integer.parseInt(st.nextToken());
+
+        int day = (length - down) / (up - down);
+        if ((length - down) % (up - down) != 0)
+            day++;
+
+        System.out.println(day);
+    }
+}
+
+
+/*
 public class Snail_2869 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
         int v = sc.nextInt();
-        int count = 0;
-        int day = 0;
 
-        int up = a - b;
+        int day = (v - b) / (a - b);
 
-
-        while (v != day) {
-            if (count != v) {
-                day++;
-                count += a;
-                if (count == v) break;
-                count -= b;
-            }
+        if ((v - b) % (a - b) != 0) {
+            day++;
         }
+
         System.out.println(day);
     }
-    /**
-     * import java.util.*;
-     * class Point{
-     * 	public int x, y;
-     * 	Point(int x, int y){
-     * 		this.x=x;
-     * 		this.y=y;
-     *        }
-     * }
-     * class Main {
-     * 	static int[] dx={-1, 0, 1, 0};
-     * 	static int[] dy={0, 1, 0, -1};
-     * 	static int[][] board, dis;
-     * 	public void BFS(int x, int y){
-     * 		Queue<Point> Q=new LinkedList<>();
-     * 		Q.offer(new Point(x, y));
-     * 		board[x][y]=1;
-     * 		while(!Q.isEmpty()){
-     * 			Point tmp=Q.poll();
-     * 			for(int i=0; i<4; i++){
-     * 				int nx=tmp.x+dx[i];
-     * 				int ny=tmp.y+dy[i];
-     * 				if(nx>=1 && nx<=7 && ny>=1 && ny<=7 && board[nx][ny]==0){
-     * 					board[nx][ny]=1;
-     * 					Q.offer(new Point(nx, ny));
-     * 					dis[nx][ny]=dis[tmp.x][tmp.y]+1;
-     *                }
-     *            }
-     *        }
-     *    }
-     *
-     * 	public static void main(String[] args){
-     * 		Main T = new Main();
-     * 		Scanner kb = new Scanner(System.in);
-     * 		board=new int[8][8];
-     * 		dis=new int[8][8];
-     * 		for(int i=1; i<=7; i++){
-     * 			for(int j=1; j<=7; j++){
-     * 				board[i][j]=kb.nextInt();
-     *            }
-     *        }
-     * 		T.BFS(1, 1);
-     * 		if(dis[7][7]==0) System.out.println(-1);
-     * 		else System.out.println(dis[7][7]);
-     *    }
-     * }
-     */
 }
-
-
+ */
