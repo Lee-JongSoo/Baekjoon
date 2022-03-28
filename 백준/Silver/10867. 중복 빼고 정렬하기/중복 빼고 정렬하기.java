43657f58ea10da84d7d2ff4f81e4b2fffba90bc0
Lee JongSu <81428357+Lee-JongSoo@users.noreply.github.com>
2022-03-28 20:24:05 +0900

    [Silver V] Title: 중복 빼고 정렬하기, Time: 356 ms, Memory: 25144 KB -BaekjoonHub

--- 백준/Silver/10867. 중복 빼고 정렬하기/중복 빼고 정렬하기.java
+++ 백준/Silver/10867. 중복 빼고 정렬하기/중복 빼고 정렬하기.java
@@ -1,24 +1,31 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
 
         HashSet<Integer> hashSet = new HashSet<>();
 
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < n; i++) {
-            hashSet.add(sc.nextInt());
+            hashSet.add(Integer.parseInt(st.nextToken()));
         }
 
         ArrayList<Integer> arrayList = new ArrayList<>(hashSet);
         Collections.sort(arrayList);
 
+        StringBuilder sb = new StringBuilder();
+
         for (int i : arrayList) {
-            System.out.print(i + " ");
+            sb.append(i).append(" ");
         }
+        System.out.println(sb.toString());
+        
     }
 }
