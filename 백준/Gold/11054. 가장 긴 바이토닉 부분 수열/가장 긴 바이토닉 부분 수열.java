346fae018a085267f78ea2da5a191a3f892c2577
Lee JongSu <81428357+Lee-JongSoo@users.noreply.github.com>
2022-03-31 11:44:34 +0900

    [Gold III] Title: 가장 긴 바이토닉 부분 수열, Time: 176 ms, Memory: 22808 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
@@ -1,74 +1,62 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
- 
+
 public class Main {
- 
-	static Integer[] r_dp;
-	static Integer[] l_dp;
-	static int[] seq;
- 
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-		int N = Integer.parseInt(br.readLine());
- 
-		r_dp = new Integer[N];	// LIS dp
-		l_dp = new Integer[N];	// LDS dp
-		seq = new int[N];
- 
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
- 
-		for (int i = 0; i < N; i++) {
-			seq[i] = Integer.parseInt(st.nextToken());
-		}
- 
-		for (int i = 0; i < N; i++) {
-			LIS(i);
-			LDS(i);
-		}
- 
-		int max = -1;
-		for (int i = 0; i < N; i++) {
-			max = Math.max(r_dp[i] + l_dp[i], max);
-		}
-		System.out.println(max - 1);
- 
-	}
- 
-	static int LIS(int N) {
- 
-		// 만약 탐색하지 않던 위치의 경우
-		if (r_dp[N] == null) {
-			r_dp[N] = 1; // 1로 초기화
- 
-			// N 이전의 노드들을 탐색
-			for (int i = N - 1; i >= 0; i--) {
-				// 이전의 노드 중 seq[N]의 값보다 작은 걸 발견했을 경우
-				if (seq[i] < seq[N]) {
-					r_dp[N] = Math.max(r_dp[N], LIS(i) + 1);
-				}
-			}
-		}
-		return r_dp[N];
-	}
- 
-	static int LDS(int N) {
-		
-		// 만약 탐색하지 않던 위치의 경우
-		if (l_dp[N] == null) {
-			l_dp[N] = 1; // 1로 초기화
- 
-			// N 이후의 노드들을 탐색
-			for (int i = N + 1; i < l_dp.length; i++) {
-				// 이후의 노드 중 seq[N]의 값보다 작은 걸 발견했을 경우
-				if (seq[i] < seq[N]) {
-					l_dp[N] = Math.max(l_dp[N], LDS(i) + 1);
-				}
-			}
-		}
-		return l_dp[N];
-	}
+
+    static Integer[] rdp;
+    static Integer[] ldp;
+    static int[] seq;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        rdp = new Integer[n];
+        ldp = new Integer[n];
+        seq = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        for (int i = 0; i < n; i++) {
+            seq[i] = Integer.parseInt(st.nextToken());
+        }
+        for (int i = 0; i < n; i++) {
+            LIS(i);
+            LDS(i);
+        }
+
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            max = Math.max(rdp[i] + ldp[i], max);
+        }
+        System.out.println(max - 1);
+    }
+
+    static int LIS(int n) {
+        if (rdp[n] == null) {
+            rdp[n] = 1;
+            for (int i = n - 1; i >= 0; i--) {
+                if (seq[i] < seq[n]) {
+                    rdp[n] = Math.max(rdp[n], LIS(i) + 1);
+                }
+            }
+        }
+        return rdp[n];
+    }
+
+    static int LDS(int n) {
+        if (ldp[n] == null) {
+            ldp[n] = 1;
+            for (int i = n + 1; i < ldp.length; i++) {
+                if (seq[i] < seq[n]) {
+                    ldp[n] = Math.max(ldp[n], LDS(i) + 1);
+                }
+            }
+        }
+        return ldp[n];
+    }
 }
