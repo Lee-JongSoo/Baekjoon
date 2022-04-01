0ab7d94387e202408780df2c00d3d8a6f0b2038e
Lee JongSu <81428357+Lee-JongSoo@users.noreply.github.com>
2022-04-01 15:30:41 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 1464 ms, Memory: 248684 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+class Edge implements Comparable<Edge> {
+
+    public int v1;
+    public int v2;
+    public int cost;
+
+    public Edge(int v1, int v2, int cost) {
+        this.v1 = v1;
+        this.v2 = v2;
+        this.cost = cost;
+    }
+
+    @Override
+    public int compareTo(Edge o) {
+        return this.cost - o.cost;
+    }
+}
+
+public class Main {
+
+    static int[] unf;
+
+    public static int Find(int v) {
+        if (v == unf[v]) return v;
+        else return unf[v] = Find(unf[v]);
+    }
+
+    public static void Union(int a, int b) {
+        int fa = Find(a);
+        int fb = Find(b);
+        if (fa != fb) unf[fa] = fb;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        unf = new int[n + 1];
+
+        ArrayList<Edge> arrayList = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            unf[i] = i;
+        }
+
+        for (int i = 0; i < m; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            arrayList.add(new Edge(a, b, c));
+        }
+
+        int answer = 0;
+        Collections.sort(arrayList);
+
+        for (Edge edge : arrayList) {
+            int fv1 = Find(edge.v1);
+            int fv2 = Find(edge.v2);
+            if (fv1 != fv2) {
+                answer += edge.cost;
+                Union(edge.v1, edge.v2);
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
