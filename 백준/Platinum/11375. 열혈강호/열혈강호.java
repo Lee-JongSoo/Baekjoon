4053003f8ba7124f87d6f5e7e8fa15872c554256
Lee JongSu <81428357+Lee-JongSoo@users.noreply.github.com>
2022-04-03 13:48:10 +0900

    [Platinum IV] Title: 열혈강호, Time: 1636 ms, Memory: 96964 KB -BaekjoonHub

---
+++ 백준/Platinum/11375. 열혈강호/열혈강호.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static List<Integer>[] list;
+    static int[] task;
+    static boolean[] check;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        list = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
+            list[i] = new ArrayList<>();
+        }
+
+        task = new int[m + 1];
+        check = new boolean[m + 1];
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int get = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < get; j++) {
+                list[i].add(Integer.parseInt(st.nextToken()));
+            }
+        }
+
+        int cnt = 0;
+        for (int i = 1; i <= n; i++) {
+            Arrays.fill(check, false);
+            if (DFS(i)) cnt++;
+        }
+
+        System.out.println(cnt);
+    }
+
+    private static boolean DFS(int ready) {
+        for (int index : list[ready]) {
+            if (check[index]) continue;
+            
+            check[index] = true;
+            if (task[index] == 0 || DFS(task[index])) {
+                task[index] = ready;
+                return true;
+            }
+        }
+        return false;
+    }
+}
