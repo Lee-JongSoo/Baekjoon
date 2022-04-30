dca1ae1e8a232c5cc442f319e753be094ab3a690
Lee JongSu <81428357+Lee-JongSoo@users.noreply.github.com>
2022-04-30 20:44:21 +0900

    [Gold V] Title: 센서, Time: 196 ms, Memory: 17260 KB -BaekjoonHub

---
+++ 백준/Gold/2212. 센서/센서.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int k = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        int[] diff = new int[n - 1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        for (int i = 0; i < n - 1; i++) {
+            diff[i] = arr[i + 1] - arr[i];
+        }
+
+        Arrays.sort(diff);
+        int res = 0;
+        for (int i = 0; i < n - k; i++) {
+            res += diff[i];
+        }
+        System.out.println(res);
+    }
+}
