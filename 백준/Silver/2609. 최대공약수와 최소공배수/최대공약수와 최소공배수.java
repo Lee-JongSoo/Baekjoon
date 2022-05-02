c549176af359ee661ecc9a2b0daf289c11d98431
Lee JongSu <81428357+Lee-JongSoo@users.noreply.github.com>
2022-05-02 11:28:49 +0900

    [Silver V] Title: 최대공약수와 최소공배수, Time: 124 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Silver/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int result = gcd(n, m);
+
+        sb.append(result).append('\n').append(n * m / result);
+        System.out.println(sb);
+    }
+
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int r = a % b;
+
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+}
