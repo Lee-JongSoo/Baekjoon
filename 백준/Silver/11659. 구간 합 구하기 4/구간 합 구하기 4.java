cf35e16ae63014412e70dbd9500a4c638442659d
Lee JongSu <81428357+Lee-JongSoo@users.noreply.github.com>
2022-06-24 17:15:24 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1424 ms, Memory: 60248 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int t = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        long sum = 0;
+        long[] longArr = new long[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            sum += arr[i];
+            longArr[i] = sum;
+        }
+
+        for (int i = 0; i < t; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (a == 1) {
+                System.out.println(longArr[b - 1]);
+            } else {
+                System.out.println(longArr[b - 1] - longArr[a - 2]);
+            }
+        }
+    }
+}
