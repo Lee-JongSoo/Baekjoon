39a2f78951eee136808ab180897cdab8f6e5d9e6
Lee JongSu <81428357+Lee-JongSoo@users.noreply.github.com>
2022-06-24 16:39:42 +0900

    [Bronze I] Title: 평균, Time: 136 ms, Memory: 14412 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -9,19 +8,20 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
 
-        double[] arr = new double[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        double max = Arrays.stream(arr).max().getAsDouble();
-
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = arr[i] / max * 100;
+        long sum = 0;
+        long max = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > max) max = arr[i];
+            sum += arr[i];
         }
 
-        System.out.println(Arrays.stream(arr).average().getAsDouble());
+        System.out.println(sum * 100.0 / max / n);
     }
 }
